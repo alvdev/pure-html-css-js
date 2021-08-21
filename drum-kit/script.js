1a798b9483426dab4c92e97a35635b028af01be9
alv.dev <soy@alv.dev>
2021-08-21 23:55:20 +0200

    Finished

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -1,4 +1,18 @@
-window.addEventListener('keydown', e => {
-  const key = e.target.getAttribute('kbd');
-  console.log(key);
-})
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`.key[data-key="${e.code}"]`);
+
+  if (audio) {
+    audio.play();
+    audio.currentTime = 0; // Rewind sound to start to allow press multiple times.
+    key.classList.add('play')
+  };
+}
+
+function removeTransition() {
+  this.classList.remove('play');
+}
+
+window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
