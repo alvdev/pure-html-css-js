f294ba2c815d5d3fd88aaed783ecb3280eb4bbf8
alvdev <soy@alv.dev>
2022-01-04 00:36:35 +0100

    Changed box colors

--- find-the-pair/script.js
+++ find-the-pair/script.js
@@ -1,25 +1,23 @@
 const colors = [
   'crimson',
   'darkorange',
-  'orange',
   'forestgreen',
-  'dodgerblue',
   'royalblue',
-  'violet',
+  'purple',
   'blueviolet',
+  'gold',
+  'violet',
 ];
 
 const h1 = document.querySelector('h1');
 const container = document.querySelector('#boxes');
-
-const changeColor = function() {
-  h1.style.color = this.style.backgroundColor;
-};
 
 for (let color of colors) {
   const box = document.createElement('div');
   box.classList.add('box');
   box.style.backgroundColor = color;
   container.appendChild(box);
-  box.addEventListener('click', changeColor);
+  box.addEventListener('click', () => {
+    h1.style.color = box.style.backgroundColor;
+  });
 }
