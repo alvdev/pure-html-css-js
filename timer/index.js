45564c7eb9f6ef9f1c14b262f48bd96beec1e510
alvdev <soy@alv.dev>
2022-01-17 00:54:48 +0100

    Calculate timeRemaining and totalDuration to fit perimeter

--- timer/index.js
+++ timer/index.js
@@ -5,9 +5,21 @@ const circle = document.querySelector('circle');
 
 const perimeter = 2 * Math.PI * circle.getAttribute('r');
 
+let currentOffset = 0;
+let duration;
+
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
+  onStart(totalDuration) {
     console.log('Timer started');
+    console.log(perimeter);
+    duration = totalDuration;
+    console.log(duration + 'el tiempo que falta');
+    circle.setAttribute('stroke-dasharray', perimeter);
+  },
+  onTick(timeRemaining) {
+    //console.log('on tick');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = (perimeter * timeRemaining) / duration - perimeter;
   },
   onPause() {
     console.log('Timer paused');
