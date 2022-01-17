eb5d5d47132b9948fe6d45d2104b55f649ffe337
alvdev <soy@alv.dev>
2022-01-17 21:47:33 +0100

    Added movies fetchData function with search params

--- movies-finder/index.js
+++ movies-finder/index.js
@@ -1,1 +1,13 @@
-console.log('Working!');
+const fetchData = async () => {
+  const response = await fetch(
+    'https://www.omdbapi.com?' +
+      new URLSearchParams({
+        apikey: '3e690ec9',
+        s: 'avengers',
+      })
+  );
+
+  console.log(await response.json());
+};
+
+fetchData();
