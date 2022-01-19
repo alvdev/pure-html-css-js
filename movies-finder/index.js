489d57906ec18713b4d52a78c12689c7fb4db0b3
alvdev <soy@alv.dev>
2022-01-19 22:48:35 +0100

    Add live searching on keypress with delay between keystrokes

--- movies-finder/index.js
+++ movies-finder/index.js
@@ -1,13 +1,22 @@
-const fetchData = async () => {
+const fetchData = async searchTerm => {
   const response = await fetch(
     'https://www.omdbapi.com?' +
       new URLSearchParams({
         apikey: '3e690ec9',
-        s: 'avengers',
+        s: searchTerm,
       })
   );
 
   console.log(await response.json());
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = input.addEventListener('keypress', e => {
+  if (timeoutId) clearTimeout(timeoutId);
+  // Avoid api queries between keystrokes lasting less 1 second
+  timeoutId = setTimeout(() => {
+    fetchData(e.target.value);
+  }, 1000);
+});
