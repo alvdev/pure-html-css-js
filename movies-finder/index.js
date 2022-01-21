c8ad7686ff0d4018e580f0b8b0782864eeda4d47
alvdev <soy@alv.dev>
2022-01-21 23:00:49 +0100

    Fixed and improved some dropdown issues

--- movies-finder/index.js
+++ movies-finder/index.js
@@ -18,15 +18,14 @@ const fetchData = async searchTerm => {
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
   <div>
-    <label for="search" class="block">
+    <label for="search">
       <strong>Search for a movies</strong>
     </label>
-    <input id="search" class="block" type="text">
+    <input id="search" class="input" type="text">
   </div>
   <div class="dropdown">
     <div class="dropdown-menu" id="dropdown-menu" role="menu">
-        <div class="dropdown-content results">
-        </div>
+        <div class="dropdown-content results"></div>
     </div>
   </div>
 `;
@@ -38,21 +37,35 @@ const results = document.querySelector('.results');
 const onInput = async e => {
   const movies = await fetchData(e.target.value);
 
-  dropdown.classList.add('is-active');
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
 
   results.innerHTML = '';
+  dropdown.classList.add('is-active');
   for (let movie of movies) {
     const option = document.createElement('a');
+    const img = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-    option.setAttribute('href', '#')
-    option.setAttribute('class', 'dropdown-item');
+    option.setAttribute('href', '#');
+    option.classList.add('dropdown-item');
     option.innerHTML = `
-      <img src="${movie.Poster}" alt="">
+      <img src="${img}" alt="">
       <h1>${movie.Title}</h1>
     `;
-    
+
     results.appendChild(option);
+
+    // Update input value with clicked movie title
+    option.addEventListener('click', () => {
+      input.value = movie.Title;
+      dropdown.classList.remove('is-active');
+    });
   }
 };
-
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', e => {
+  if (!root.contains(e.target)) dropdown.classList.remove('is-active');
+});
