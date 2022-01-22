993f58691b71e4d85363f2aa96b1418bf7b936b0
alvdev <soy@alv.dev>
2022-01-22 03:45:29 +0100

    Added movie info when option selected and refactoring autocomplete

--- movies-finder/index.js
+++ movies-finder/index.js
@@ -15,60 +15,14 @@ const fetchData = async searchTerm => {
   return results.Search;
 };
 
-const root = document.querySelector('#autocomplete');
-root.innerHTML = `
-  <div>
-    <label for="search">
-      <strong>Search for a movies</strong>
-    </label>
-    <input id="search" class="input" type="text">
-  </div>
-  <div class="dropdown">
-    <div class="dropdown-menu" id="dropdown-menu" role="menu">
-        <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const results = document.querySelector('.results');
-
-const onInput = async e => {
-  const movies = await fetchData(e.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-
-  results.innerHTML = '';
-  dropdown.classList.add('is-active');
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    const img = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.setAttribute('href', '#');
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-      <img src="${img}" alt="">
-      <h1>${movie.Title}</h1>
-    `;
-
-    results.appendChild(option);
-
-    // Update input value with clicked movie title
-    option.addEventListener('click', () => {
-      input.value = movie.Title;
-      dropdown.classList.remove('is-active');
-      onMovieSelect(movie);
-    });
-  }
-};
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', e => {
-  if (!root.contains(e.target)) dropdown.classList.remove('is-active');
+createAutocomplete({
+  root: document.querySelector('.autocomplete'),
+});
+createAutocomplete({
+  root: document.querySelector('.autocomplete-2'),
+});
+createAutocomplete({
+  root: document.querySelector('.autocomplete-3'),
 });
 
 const onMovieSelect = async movie => {
@@ -76,11 +30,11 @@ const onMovieSelect = async movie => {
     'https://www.omdbapi.com?' +
       new URLSearchParams({
         apikey: '3e690ec9',
-        i: 'tt1285016',
+        i: movie.imdbID,
       })
   );
 
-  document.querySelector('#summary').innerHTML = movieTemplate(
+  document.querySelector('.summary').innerHTML = movieTemplate(
     await response.json()
   );
 };
@@ -100,6 +54,31 @@ const movieTemplate = movieInfo => {
           <p>${movieInfo.Plot}</p>
         </div>
       </div>
+    </article>
+    
+    <article class="notification is-primary">
+      <p class="title">${movieInfo.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+
+    <article class="notification is-primary">
+      <p class="title">${movieInfo.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+
+    <article class="notification is-primary">
+      <p class="title">${movieInfo.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+
+    <article class="notification is-primary">
+      <p class="title">${movieInfo.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+
+    <article class="notification is-primary">
+      <p class="title">${movieInfo.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
     </article>
   `;
 };
