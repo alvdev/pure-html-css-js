554b9ceb75bfcd28968a6bfccf165301e4b5a279
alv.dev <soy@alv.dev>
2023-02-16 20:14:08 +0100

    select neccessary elements to interact with

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,7 @@
+'use strict';
+
+// Select neccessary elements to interact
+const again = document.querySelector('button.again');
+const check = document.querySelector('button.check');
+const guess = document.querySelector('input.guess');
 
