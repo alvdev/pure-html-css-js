27df9325283b322c98beed9ffd689c17c9be4f85
alv.dev <soy@alv.dev>
2023-02-16 21:46:36 +0100

    feat: add guess number

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -4,6 +4,25 @@
 const again = document.querySelector('button.again');
 const check = document.querySelector('button.check');
 const guess = document.querySelector('input.guess');
+const message = document.querySelector('.message');
+
+let score = document.querySelector('.score');
+let scoreNumber = +score.textContent;
+
+const placeholder = document.querySelector('.number');
 
 // Number to guess between 1 and 20
 const number = Math.trunc(Math.random() * 20) + 1;
+
+check.addEventListener('click', () => {
+  if (!guess.value) {
+    message.textContent = 'Enter a number';
+  } else if (+guess.value !== number) {
+    scoreNumber--;
+    score.textContent = scoreNumber <= 0 ? 'You lost' : scoreNumber;
+    guess.disabled = scoreNumber <=0 ? true : false;
+  } else if (+guess.value === number) {
+    message.textContent = 'You guessed it!';
+    document.body.style.backgroundColor = 'forestgreen';
+  }
+})
