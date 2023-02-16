7874a4cad44d311771b0d0d68e0078310591cf05
alv.dev <soy@alv.dev>
2023-02-16 20:20:18 +0100

    declare a random number between 1 and 20

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -5,3 +5,5 @@ const again = document.querySelector('button.again');
 const check = document.querySelector('button.check');
 const guess = document.querySelector('input.guess');
 
+// Number to guess between 1 and 20
+const number = Math.trunc(Math.random() * 20) + 1;
