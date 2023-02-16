6156afd53b9e5fc417b66be6f32461a3e593c1a3
alv.dev <soy@alv.dev>
2023-02-16 21:55:45 +0100

    feat: add too high or too high tips

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -13,6 +13,7 @@ const placeholder = document.querySelector('.number');
 
 // Number to guess between 1 and 20
 const number = Math.trunc(Math.random() * 20) + 1;
+// placeholder.textContent = number;
 
 check.addEventListener('click', () => {
   if (!guess.value) {
@@ -20,7 +21,8 @@ check.addEventListener('click', () => {
   } else if (+guess.value !== number) {
     scoreNumber--;
     score.textContent = scoreNumber <= 0 ? 'You lost' : scoreNumber;
-    guess.disabled = scoreNumber <=0 ? true : false;
+    message.textContent = guess.value > number ? 'Too high' : 'Too low';
+    guess.disabled = scoreNumber <= 0 ? true : false;
   } else if (+guess.value === number) {
     message.textContent = 'You guessed it!';
     document.body.style.backgroundColor = 'forestgreen';
