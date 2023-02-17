7a52309ee15d5f013f2ca16b4f1856e23cef3ad2
alv.dev <soy@alv.dev>
2023-02-17 19:45:53 +0100

    refactor: add show/hide modal functions

--- modal/script.js
+++ modal/script.js
@@ -1,26 +1,24 @@
 'use strict';
 
 const modalBtns = document.querySelectorAll('.show-modal');
-const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close-modal');
-const overlay = document.querySelector('.overlay');
 
-modalBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-});
+const showModal = () => {
+  document.querySelector('.modal').classList.remove('hidden');
+  document.querySelector('.overlay').classList.remove('hidden');
+};
 
-closeBtn.addEventListener('click', () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+const hideModal = () => {
+  document.querySelector('.modal').classList.add('hidden');
+  document.querySelector('.overlay').classList.add('hidden');
+}
 
+// Show modal
+for (let btn of modalBtns) btn.addEventListener('click', showModal);
+
+// Hide modal
+closeBtn.addEventListener('click', hideModal);
 document.addEventListener('keydown', e => {
-  if (e.key === 'Escape') {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  };
-})
+  if (e.key === 'Escape') hideModal();
+});
 
