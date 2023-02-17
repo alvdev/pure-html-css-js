01882fa22ebb9e47d92b5aa212becfbf675e3daa
alv.dev <soy@alv.dev>
2023-02-17 18:57:24 +0100

    feat: open/close modal with bg blur overlay

--- modal/script.js
+++ modal/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const modalBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+modalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
