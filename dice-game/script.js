b8638680e9615ab649133c6aad2865f0859071f9
alv.dev <soy@alv.dev>
2023-02-19 14:41:38 +0100

    feat: roll dice funcionality

--- dice-game/script.js
+++ dice-game/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Roll dice
+const rollBtn = document.querySelector('.btn--roll');
+const dice = document.querySelector('.dice');
+
+rollBtn.addEventListener('click', () => {
+  const diceNum = Math.trunc(Math.random() * 6) + 1;
+  dice.src = `dice-${diceNum}.png`;
+  console.log('This is working');
+})
